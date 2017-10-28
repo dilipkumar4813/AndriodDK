3280f5dc36eb6880ded60adf9b0828d484e73fd6
Dilip <dilipkumar4813@gmail.com>
2017-10-28 16:27:28 +0530

    Implementation of image flip using library

--- app/src/main/java/iamdilipkumar/com/andrioddk/ui/activities/FlipPageActivity.java
+++ app/src/main/java/iamdilipkumar/com/andrioddk/ui/activities/FlipPageActivity.java
@@ -2,12 +2,16 @@ package iamdilipkumar.com.andrioddk.ui.activities;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import app.num.numandroidpagecurleffect.PageCurlView;
 import iamdilipkumar.com.andrioddk.R;
-import iamdilipkumar.com.andrioddk.ui.fragments.FirstFragment;
 
 public class FlipPageActivity extends AppCompatActivity {
 
@@ -15,7 +19,20 @@ public class FlipPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_page);
-        changeFragment(new FirstFragment());
+
+        PageCurlView pageCurlView = (PageCurlView) findViewById(R.id.pagecurl_view);
+
+        List<Integer> pages_id = new ArrayList<>();
+        pages_id.add(R.drawable.star_wars_3);
+        pages_id.add(R.drawable.star_wars_2);
+
+        pageCurlView.setCurlView(pages_id);
+        pageCurlView.setCurlSpeed(65);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.hide();
+        }
     }
 
     private void changeFragment(Fragment fragment) {
---
+++ app/src/main/java/iamdilipkumar/com/andrioddk/utils/Constants.java
@@ -0,0 +1,15 @@
+package iamdilipkumar.com.andrioddk.utils;
+
+/**
+ * Class containing the constants for the page flip
+ *
+ * @author dilipkumar4813
+ * @version 1.0
+ * @since 28/10/17
+ */
+
+public class Constants {
+    public final static String PREF_MESH_PIXELS = "MeshPixels";
+    public final static String PREF_DURATION    = "Duration";
+    public final static String PREF_PAGE_MODE   = "PageMode";
+}
