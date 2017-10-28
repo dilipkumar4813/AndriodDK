eca448510ff9790bf7d470bcf7f5f89e20c5880f
Dilip <dilipkumar4813@gmail.com>
2017-10-28 13:14:08 +0530

    Initial commit

---
+++ app/src/main/java/iamdilipkumar/com/andrioddk/FlipPageActivity.java
@@ -0,0 +1,13 @@
+package iamdilipkumar.com.andrioddk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlipPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flip_page);
+    }
+}
